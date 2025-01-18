cd1407bd484d83c7577a417ab2971088f1524f77
Thijs Van Den Berg <thivan-d@f1r6s17.codam.nl>
2025-01-18 14:57:47 +0100

    libft

--- libft/ft_atof.c
+++ libft/ft_atof.c
@@ -1,42 +1,24 @@
 #include "libft.h"
-#include <ctype.h>
-#include <stdlib.h>
 
 double	ft_atof(const char *str)
 {
-	double	result;
-	double	fraction;
-	int		sign;
-	int		i;
-	double	divisor;
+	double	res;
+	double	res2;
+	char	*c;
+	int		len;
 
-	result = 0.0;
-	fraction = 0.0;
-	sign = 1;
-	i = 0;
-	while (isspace(str[i]))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (isdigit(str[i]))
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	if (str[i] == '.')
-	{
-		i++;
-		divisor = 10.0;
-		while (isdigit(str[i]))
-		{
-			fraction += (str[i] - '0') / divisor;
-			divisor *= 10.0;
-			i++;
-		}
-	}
-	return (sign * (result + fraction));
+	c = (char *)str;
+	res = (double)ft_atoi(c);
+	while (*c && *c != '.')
+		c++;
+	if (*c == '.')
+		c++;
+	res2 = (double)ft_atoi(c);
+	len = ft_strlen(c);
+	while (len--)
+		res2 /= 10;
+	if (res >= 0)
+		return (res + res2);
+	else
+		return (res + -res2);
 }
